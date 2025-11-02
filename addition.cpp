b99b270c2bfa8d144a61601ff646b42ae5916b91
pavanshinde1232 <pavanshinde2704@gmail.com>
2025-11-02 12:15:47 +0530

    additon of two numbers

---
+++ addition.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a , b , c;
+    cout << "enter value of a:" ;
+    cin >> a;
+    cout << "enter value of b:"; 
+    cin >> b;
+    c = a + b ;
+    cout << "the additon is :" << c << endl;
+    return 0;
+
+}
